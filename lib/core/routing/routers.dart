2c4b83251184c979edb0053424acc24971fb2e29
Tantawii <ahmedtramadan4@gmail.com>
2024-06-12 07:55:26 +0300

    - change a basic splash_screen. - use flutter_native_splash lib - added a image splash logo  for android12 - added image splash for Ios & android below 12.

--- lib/core/routing/routers.dart
+++ lib/core/routing/routers.dart
@@ -3,7 +3,6 @@
 class Routes{
 
   static const String onBoardingScreen = "/onBoardingScreen";
-  static const String splashScreen = "/splashScreen";
   static const String loginScreen = "/loginScreen";
 
 }
